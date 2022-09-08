2629049b66b43f173e95407ebebaf7814f5085f7
mateuso12 <mateuso12@hotmail.com.br>
2022-09-08 00:59:32 -0300

    Create reducer method from cycles actions

---
+++ src/reducers/cycles/actions.ts
@@ -0,0 +1,28 @@
+import { Cycle } from './reducer'
+
+export enum ActionCyclesTypes {
+  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
+  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
+  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
+}
+
+export function addNewCycleAction(newCycle: Cycle) {
+  return {
+    type: ActionCyclesTypes.ADD_NEW_CYCLE,
+    payload: {
+      newCycle,
+    },
+  }
+}
+
+export function interruptCurrentCycleAction() {
+  return {
+    type: ActionCyclesTypes.INTERRUPT_CURRENT_CYCLE,
+  }
+}
+
+export function markCurrentCycleAsFinishedAction() {
+  return {
+    type: ActionCyclesTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
+  }
+}
